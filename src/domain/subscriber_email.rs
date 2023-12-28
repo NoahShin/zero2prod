05fe105fb2bcd98aec83ea60609f71468286c274
shinjaehoon <noahshin11@gmail.com>
2023-12-28 15:45:00 +0900

    ch.9 done

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -59,3 +59,11 @@ mod tests {
         SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
+
+impl std::fmt::Display for SubscriberEmail {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // We just forward to the Display implementation of
+        // the wrapped String.
+        self.0.fmt(f)
+    }
+}
